68467d508822fadd0af154ab7ba1b9841982c5e4
jonathanyu1 <jonathanyusir@gmail.com>
2021-04-02 17:52:54 -0400

    finished initial version

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -52,21 +52,31 @@ const Cart = (props) => {
     // add section for empty cart
     return (
         <div id='cartContainer'>
-            <div id='cartTitle'>Shopping Cart</div>
-                {cartItems.map(cartItem=>{
-                    return (
-                        <CartItem
-                            key={cartItem.id}
-                            cartItem={cartItem}
-                            changeProductQuantity={props.changeProductQuantity}
-                            removeProduct={props.removeProduct}
-                        />
-                    )
-                })}
-                <div id='cartTotalContainer'>
-                    <div id='cartSubtotal'>Subtotal: {props.subtotal}</div>
-                    <Link to='/'>Checkout</Link>
+            {cartItems.length>0 ? 
+                <React.Fragment>
+                    <div id='cartTitle'>Shopping Cart</div>
+                    {cartItems.map(cartItem=>{
+                        return (
+                            <CartItem
+                                key={cartItem.id}
+                                cartItem={cartItem}
+                                changeProductQuantity={props.changeProductQuantity}
+                                removeProduct={props.removeProduct}
+                            />
+                        )
+                    })}
+                    <div id='cartTotalContainer'>
+                        <div id='cartSubtotal'>Subtotal: {props.subtotal}</div>
+                        <Link to='/'>Checkout</Link>
                 </div>
+                </React.Fragment>
+                : 
+                <div id='cartEmptyContainer'>
+                    <div id='cartEmptyMessage'>Your cart is empty.</div>
+                    <span class="material-icons">shopping_bag</span>
+                </div> 
+            }
+            
         </div>
     )
 }
